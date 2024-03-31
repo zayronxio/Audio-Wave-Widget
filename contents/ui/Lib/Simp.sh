e3fc2784bb96f5cf84b5cd6b6784acdf7a65329c
zayronxio <zayronxio@gmail.com>
2024-03-31 14:41:43 -0600

    removed ffw3 dependency
    The audio_analyzer command was completely changed to not use the fftw3 library and limit the use of portaudio. In most cases, extra installations of dependencies will not be necessary, once the command is compiled.

--- contents/ui/Lib/Simp.sh
+++ contents/ui/Lib/Simp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-command="$($HOME/.local/share/plasma/plasmoids/Audio.Wave.Widget/contents/ui/Lib/audio_analyzer2 2>/dev/null | grep "Frecuencia:" | sed 's/Frecuencia://g' | sed 's/ dB//g' )"
-elimEtiquetas="$(echo "$command" | sed 's/20 Hz - 1346 Hz: //g' | sed 's/1346 Hz - 2672 Hz: //g' | sed 's/2672 Hz - 3998 Hz: //g' | sed 's/4000 Hz - 5333 Hz: //g' | sed 's/5333 Hz - 6666 Hz: //g' | sed 's/5333 Hz - 6666 Hz: //g'  | sed 's/6666 Hz - 7999 Hz: //g' | sed 's/7999 Hz - 9332 Hz: //g' | sed 's/9332 Hz - 10665 Hz: //g' | sed 's/10665 Hz - 11998 Hz: //g')"
+command="$($HOME/.local/share/plasma/plasmoids/Audio.Wave.Widget/contents/ui/Lib/audio_analyzer 2>/dev/null | grep "Frecuencia:" | sed 's/Frecuencia://g' | sed 's/ dB//g' )"
+elimEtiquetas="$(echo "$command" | sed 's/20 Hz - 121 Hz: //g' | sed 's/121 Hz - 501 Hz: //g' | sed 's/501 Hz - 901 Hz: //g' | sed 's/901 Hz - 1421 Hz: //g' | sed 's/1421 Hz - 2001 Hz: //g' | sed 's/2001 Hz - 3001 Hz: //g'  | sed 's/3001 Hz - 4351 Hz: //g' | sed 's/4351 Hz - 6801 Hz: //g' | sed 's/6801 Hz - 13000 Hz: //g')"
 elimSaltos="$(echo "$elimEtiquetas" | sed ':a;N;$!ba;s/\n/ /g' )"
 echo "$elimSaltos"
